eb074e772f2cbf119cb338760db001fd5e208f1b
Zbytes <111581344+zbytes2227@users.noreply.github.com>
2024-06-09 08:36:09 +0530

    Update AppFooterCopyright.jsx

--- components/shared/AppFooterCopyright.jsx
+++ components/shared/AppFooterCopyright.jsx
@@ -2,16 +2,16 @@ function AppFooterCopyright() {
 	return (
 		<div className="font-general-regular flex justify-center items-center text-center">
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
-			
+			Made with ❤
 				<a
-					href=""
+					href="/about"
 					target="__blank"
 					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
-					Made with ❤
+		{' '}by @Aayushi
 				</a>
-				{' '}
-				by Aayushi
+				
+				
 			</div>
 		</div>
 	);
