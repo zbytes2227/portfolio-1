9e7448c32a19f1fe46ea799b44a93098613db0df
Zbytes <111581344+zbytes2227@users.noreply.github.com>
2024-06-09 08:32:15 +0530

    Update hbd.jsx

--- pages/hbd.jsx
+++ pages/hbd.jsx
@@ -12,7 +12,7 @@ const hbd = () => {
   <div class="container mx-auto flex px-5 py-12 items-center justify-center flex-col">
     <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="/images/gif1.gif"/>
     <div class="text-center lg:w-2/3 w-full">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">🎉✨ Happy Birthday 🥳🎂
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">🎉✨ Happy Birthday Didi🥳🎂
     </h1>
       <p class="mb-8 leading-relaxed">Click on the below button to reveal your gift !</p>
       <div class="flex justify-center">
